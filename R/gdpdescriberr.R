5c94097ef4cec67aac09524407a34dd0969396bc
GloriaWYY <wanying.ye2020@gmail.com>
2022-01-21 17:51:09 -0800

    Fix describerr

--- R/gdpdescriberr.R
+++ R/gdpdescriberr.R
@@ -1,4 +1,4 @@
-#' GDP describe
+#' GDP describerr
 #' Calculates summary statistics for the Numeric Variable x grouping by categorical variable y.
 #'     The function is able to calculate the following descriptive statistics:
 #'Mean
@@ -14,14 +14,14 @@
 #' @param data.frame cleaned data frame preprocessed by gdpcleanerr
 #' @param x  column name of a data frame used to calculate the descriptive statistics
 #' @param y  column name of a grouping variable
-#' @param stats vector with the descriptive stats to calculate 
-#' @param dec   number of decimal places to return in the table 
+#' @param stats vector with the descriptive stats to calculate
+#' @param dec   number of decimal places to return in the table
 #'
 #' @return
 #' @export
 #'
-#' @examples 
-#' gdpdescribe(df, "GDP", "year", stats=c("mean", "median", "sd", "min", "max", "range_"), dec=2)
-gdpdescriber  <- function(data.frame, x, y, stats=c("mean", "sd", "median"), dec=2){
-  
+#' @examples
+#' gdpdescriberr(df, "GDP", "year", stats=c("mean", "median", "sd", "min", "max", "range_"), dec=2)
+gdpdescriberrr  <- function(data.frame, x, y, stats=c("mean", "sd", "median"), dec=2){
+
 }
