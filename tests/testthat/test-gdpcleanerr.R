1d2fbbd176fb337262548fabd79d36bae50230df
Gabe Fairbrother <gfairbrother@gmail.com>
2022-01-25 15:39:06 -0800

    test and importer tweaks

--- tests/testthat/test-gdpcleanerr.R
+++ tests/testthat/test-gdpcleanerr.R
@@ -1,14 +1,14 @@
 
 test_that("gdpcleanerr throws an error if not called with any argument",
-           expect_error(gdpcleanerr()))
+           {expect_error(gdpcleanerr())})
 
 test_that("gdpcleanerr throws an error if called with a non-dataframe arguement",
-           expect_error(gdpcleaner("Hello")))
+           {expect_error(gdpcleaner("Hello"))})
 
 
 test_that("gdpcleanerr returns a dataframe",
           {
-            df = readr::read_csv("test_13100347.csv")
+            df = readr::read_csv("test_13100347.csv") |> suppressMessages() |> suppressWarnings()
             out_df = gdpcleanerr(df)
             expect_s3_class(out_df, "data.frame")
 
@@ -17,7 +17,7 @@ test_that("gdpcleanerr returns a dataframe",
 
 test_that("gdpcleanerr has at least 5 columns",
           {
-            df = readr::read_csv("test_13100347.csv")
+            df = readr::read_csv("test_13100347.csv") |> suppressMessages() |> suppressWarnings()
             out_df = gdpcleanerr(df)
            expect_true(ncol(out_df) >= 5)
           }
@@ -25,7 +25,7 @@ test_that("gdpcleanerr has at least 5 columns",
 
 test_that("gdpcleanerr does not return an empty dataframe",
           {
-            df = readr::read_csv("test_13100347.csv")
+            df = readr::read_csv("test_13100347.csv") |> suppressMessages() |> suppressWarnings()
             out_df = gdpcleanerr(df)
             expect_false(nrow(out_df) == 0)
           }
