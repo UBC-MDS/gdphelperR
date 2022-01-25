b6069406a7a68694d9f36a873abfac1c6baceb4e
GloriaWYY <wanying.ye2020@gmail.com>
2022-01-24 20:46:16 -0800

    Modify docstring, add toy example

--- R/gdpdescriberr.R
+++ R/gdpdescriberr.R
@@ -20,24 +20,28 @@
 #' @return data.frame
 #' @export
 #'
-#' @examples gdpdescriberr(df1, Value, Sex, .stats=c("mean", "sd", "median"), dec = 2)
+#' @examples
+#' df1 <- tibble::tribble(~REF_DATE,~GEO,~DGUID, ~`North.American.Industry.Classification.System.(NAICS)`,      ~UOM, ~UOM_ID, ~SCALAR_FACTOR, ~SCALAR_ID,~VECTOR, ~COORDINATE,~VALUE,~STATUS,~SYMBOL,~TERMINATED,~DECIMALS,1997L, "Canada", "2016A000011124",                                       "Total industries", "Dollars",     81L,     "millions",         6L, "v41894235",1.477, 816756.1, NA,NA,  NA,1L,1997L,"Canada","2016A000011124", "Crop and animal production", "Dollars",81L,"millions",6L,"v41700857", 1.1,12755.9,NA,NA, NA,1L)
+#' gdpdescriberr(df1, VALUE, GEO, .stats=c("mean", "sd", "median"), dec = 2)
+
+
 gdpdescriberr = function(df1, x, y, .stats=c("mean", "sd", "median"), dec = 2){
-  library(dplyr)
+  # library(dplyr)
   options(warn = -1)
 
   if(!is.data.frame(df1)){
     stop("The object is not a dataframe")
   }
 
-  if (df1 |> select({{y}}) |> dplyr::n_distinct() >=nrow(df1)){
+  if (df1 |> dplyr::select({{y}}) |> dplyr::n_distinct() >=nrow(df1)){
     stop("Error: Variable Y has values in every row")
   }
 
-  if(!is.numeric(df1 |> select({{x}}) |> pull())){
+  if(!is.numeric(df1 |> dplyr::select({{x}}) |> dplyr::pull())){
     stop("Variable X is not numeric")
   }
 
-  if(!is.character(df1 |> select({{y}}) |> pull())){
+  if(!is.character(df1 |> dplyr::select({{y}}) |> dplyr::pull())){
     stop("Variable Y is not categorical")
   }
 
@@ -47,40 +51,40 @@ gdpdescriberr = function(df1, x, y, .stats=c("mean", "sd", "median"), dec = 2){
   }
 
   e1 = df1 |>
-    group_by({{ y }}) |>
-    summarise( mean := mean({{ x }},  na.rm = T))
+    dplyr::group_by({{ y }}) |>
+    dplyr::summarise( mean := mean({{ x }},  na.rm = T))
 
   e2 = df1 |>
-    group_by({{ y }}) |>
-    summarise( median := median({{ x }},  na.rm = T))
+    dplyr::group_by({{ y }}) |>
+    dplyr::summarise( median := median({{ x }},  na.rm = T))
 
   e3 = df1 |>
-    group_by({{ y }}) |>
-    summarise( sd := sd({{ x }},  na.rm = T))
+    dplyr::group_by({{ y }}) |>
+    dplyr::summarise( sd := stats::sd({{ x }},  na.rm = T))
 
   e4 = df1 |>
-    group_by({{ y }}) |>
-    summarise( min := min({{ x }},  na.rm = T))
+    dplyr::group_by({{ y }}) |>
+    dplyr::summarise( min := min({{ x }},  na.rm = T))
 
   e5 = df1 |>
-    group_by({{ y }}) |>
-    summarise( max := max({{ x }},  na.rm = T))
+    dplyr::group_by({{ y }}) |>
+    dplyr::summarise( max := max({{ x }},  na.rm = T))
 
   e6 = df1 |>
-    group_by({{ y }}) |>
-    summarise( range :=  max({{ x }}, na.rm = T) - min({{ x }}, na.rm = T))
+    dplyr::group_by({{ y }}) |>
+    dplyr::summarise( range :=  max({{ x }}, na.rm = T) - min({{ x }}, na.rm = T))
 
-  e7 = df1 |> group_by({{ y }}) |>
-    summarise(q75 := quantile({{ x }}, .75, na.rm=T))
+  e7 = df1 |> dplyr::group_by({{ y }}) |>
+    dplyr::summarise(q75 := stats::quantile({{ x }}, .75, na.rm=T))
 
-  e8 = df1 |> group_by({{ y }}) |>
-    summarise(q25 := quantile({{ x }}, .25, na.rm=T))
+  e8 = df1 |> dplyr::group_by({{ y }}) |>
+    dplyr::summarise(q25 := stats::quantile({{ x }}, .25, na.rm=T))
 
-  e9 = df1 |> group_by({{ y }}) |>
-    summarise(iqr := IQR({{ x }}, na.rm=T))
+  e9 = df1 |> dplyr::group_by({{ y }}) |>
+    dplyr::summarise(iqr := stats::IQR({{ x }}, na.rm=T))
 
-  e10 = df1 |> group_by({{ y }}) |>
-    summarise(nas = sum(is.na({{x}})))
+  e10 = df1 |> dplyr::group_by({{ y }}) |>
+    dplyr::summarise(nas = sum(is.na({{x}})))
 
   .all.stats = c('mean', 'median', 'sd', 'min', 'max', 'range' , "q75", "q25", "iqr", "nas")
   .all.stats.o = paste0("e", 1:length(.all.stats))
@@ -94,6 +98,6 @@ gdpdescriberr = function(df1, x, y, .stats=c("mean", "sd", "median"), dec = 2){
     .res = cbind(.res,.r)
   }
 
-  return(as_tibble(cbind(colnames(.res),t(.res))) )
+  return(tibble::as_tibble(cbind(colnames(.res),t(.res))) )
   options(warn = getOption("warn"))
 }
