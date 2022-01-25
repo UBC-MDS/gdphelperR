28a67005f17e781ed23266b836de4c79b5a1c6d8
Gabe Fairbrother <gfairbrother@gmail.com>
2022-01-25 14:54:52 -0800

    Code Complete, tests not passed

--- R/gdpcleanerr.R
+++ R/gdpcleanerr.R
@@ -14,3 +14,55 @@ gdpcleanerr <- function(data.frame){
 
 
 }
+#' GDP Cleaner
+#' Author: Gabe Fairbrother
+#'This function will strip spurious columns, and Null valued rows, and rename retained columns
+#'from any data set from the Open Government Canada Portal that indicates it is a
+#'"GDP at Basic Prices" data set (see Data_Sources.csv for examples).
+#'
+#' @param data.frame A data frame generated from the Open Government Canada Portal, preferably imported by gdpimporter
+#'
+#' @return data.frame A cleaned and simplified data frame
+#' @export
+#'
+#' @examples
+#' df <- gdpcleanerr(data)
+gdpcleanerr <- function(gdpdata){
+
+    #Check that the argument is correct
+    if (is.null(gdpdata)) {
+        stop("No argument. Please call gdpcleanerr with a dataframe as the only argument")
+    }
+    if (!is.data.frame(gdpdata)) {
+        stop("Please call gdpcleaner with a dataframe as the only argument")
+    }
+
+    #Clear problem names
+    names(gdpdata)<-stringr::str_replace_all(names(gdpdata), c(" " = "_" , "," = "_" ))
+
+    #Remove spurious columns and NAs, and rename kept columns
+    cleaned_data <- gdpdata |> dplyr::select(-one_of(c("DGUID", "UOM_ID", "SCALAR_ID",
+                                                 "VECTOR", "COORDINATE", "STATUS",
+                                                  "SYMBOL", "TERMINATED", "DECIMALS",
+                                                  "Value", "Seasonal_adjustment"))) |>
+                                stats::na.omit() |>
+                                dplyr::rename(Date = REF_DATE , Location = GEO,
+                                    Scale = SCALAR_FACTOR, Value = VALUE,
+                                    Unit = UOM) |> suppressWarnings()
+
+
+    #Check for NAICS and Aggregates columns
+    naics_index <- grep('NAICS', colnames(cleaned_data))
+    aggs_index <- grep('aggregat', colnames(cleaned_data))
+
+    #Rename if found
+    if (length(naics_index) > 0) {
+        cleaned_data <- dplyr::rename(cleaned_data, NAICS_Class = naics_index)
+    }
+
+    if (length(aggs_index) > 0) {
+      cleaned_data <- dplyr::rename(cleaned_data, Special_Industry = aggs_index)
+    }
+#Return clean dataframe
+cleaned_data
+}
